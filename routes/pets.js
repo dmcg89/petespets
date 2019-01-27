70690511b72e1fc5c61e3db918f0fa3c3fbbf341
Drew McGowan <michael.mcgowan@students.makeschool.com>
2019-01-26 22:50:58 -0800

    implemented search pagination

--- routes/pets.js
+++ routes/pets.js
@@ -58,14 +58,19 @@ module.exports = (app) => {
 
   // SEARCH PET
   app.get('/search', (req, res) => {
-    term = new RegExp(req.query.term, 'i')
+    const term = new RegExp(req.query.term, 'i')
+    const page = req.query.page || 1
 
-    Pet.find({$or:[
-      {'name': term},
-      {'species': term}
-    ]}).exec((err, pets) => {
-      res.render('pets-index', { pets: pets });
-    })
+    Pet.paginate(
+      {
+        $or: [
+          { 'name': term },
+          { 'species': term }
+        ]
+      },
+      { page: page }).then((results) => {
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+      });
   });
 
 
