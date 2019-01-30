5183523c161f072e3b1dc365c2e2257e52e5bc38
Drew McGowan <michael.mcgowan@students.makeschool.com>
2019-01-29 19:17:36 -0800

    aws almost working?

--- routes/pets.js
+++ routes/pets.js
@@ -1,6 +1,34 @@
 // MODELS
 const Pet = require('../models/pet');
 
+//uploading to aws s3
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Upload = require('s3-uploader');
+
+const client = new Upload(process.env.S3_BUCKET, {
+  aws: {
+    path: 'pets/avatar',
+    region: process.env.S3_REGION,
+    acl: 'public-read',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+  cleanup: {
+    versions: true,
+    original: true
+  },
+  versions: [{
+    maxWidth: 400,
+    aspect: '16:10',
+    suffix: '-standard'
+  },{
+    maxWidth: 300,
+    aspect: '1:1',
+    suffix: '-square'
+  }]
+});
+
 // PET ROUTES
 module.exports = (app) => {
 
@@ -12,19 +40,28 @@ module.exports = (app) => {
   });
 
   // CREATE PET
-  app.post('/pets', (req, res) => {
-    console.log(req.body);
-    var pet = new Pet(req.body);
+    app.post('/pets', upload.single('avatar'), (req, res, next) => {
+      var pet = new Pet(req.body);
+      pet.save(function (err) {
+        if (req.file) {
+          client.upload(req.file.path, {}, function (err, versions, meta) {
+            if (err) { return res.status(400).send({ err: err }) };
 
-    pet.save()
-      .then((pet) => {
-        res.send({ pet: pet });
+            versions.forEach(function (image) {
+              var urlArray = image.url.split('-');
+              urlArray.pop();
+              var url = urlArray.join('-');
+              pet.avatarUrl = url;
+              pet.save();
+            });
+
+            res.send({ pet: pet });
+          });
+        } else {
+          res.send({ pet: pet });
+        }
       })
-      .catch((err) => {
-        // STATUS OF 400 FOR VALIDATIONS
-        res.status(400).send(err.errors);
-      }) ;
-  });
+    })
 
   // SHOW PET
   app.get('/pets/:id', (req, res) => {
